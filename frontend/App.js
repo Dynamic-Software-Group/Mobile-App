d8c919aec326faffd1817454f19886d657fcbd58
Incbom <99501939+IncbomDev@users.noreply.github.com>
2023-12-18 15:14:06 -0500

    Changes by rohanmishra

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View} from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { useFonts } from 'expo-font';
-import {SearchOutlined} from "@ant-design/icons";
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -15,12 +15,12 @@ export default function App() {
         <Text style={{left: wp('3%'), top: hp('1.5%'), fontWeight: 'medium', fontSize: 32}}>Near You</Text>
         <View style={{left: wp('2.5%'), right: wp('2.5%'), top: hp('2%'), backgroundColor: '#D9D9D9', height: hp('0.4%'), width: wp('95%')}}></View>
         <View style={{left: wp('4%'), right: wp('4%'), backgroundColor: '#F3F8FE', height: hp('5%'), top: hp('3%'), width: wp('92%'), borderRadius: wp('3.5%'), display: 'flex', justifyContent: 'center'}}>
-            {/*<SearchOutlined style={{}} />*/}
-            <Text style={{color: 'gray', marginLeft: '10px'}}>Find stores</Text>
+            <AntDesign name="search1" size={14} color="black" />
+            <Text style={{left: wp('10%'), color: 'gray', marginLeft: '10px'}}>Find stores</Text>
         </View>
         <View>
             <View style={{top: hp('3%'), left: wp('4%'), display: 'flex'}}>
-                <View style={{top: hp('10%'), left: wp('4%'), width: wp('33%'), height: hp('7%'), backgroundColor: 'blue'}}>
+                <View style={{top: hp('1.5%'), left: wp('4%'), width: wp('25%'), height: hp('3%'), backgroundColor: '#99bb88'}}>
 
                 </View>
             </View>
