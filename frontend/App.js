38c16acb551d0e9bc9fed3444e26dd19c8e96906
Incbom <99501939+IncbomDev@users.noreply.github.com>
2023-12-18 20:53:17 -0500

    commit

--- frontend/App.js
+++ frontend/App.js
@@ -1,51 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Image, SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { useFonts } from 'expo-font';
 import { AntDesign } from '@expo/vector-icons';
+import { useFonts, Montserrat_100Thin, Montserrat_100Thin_Italic, Montserrat_300Light_Italic, Montserrat_200ExtraLight, Montserrat_400Regular_Italic, Montserrat_500Medium_Italic, Montserrat_200ExtraLight_Italic, Montserrat_800ExtraBold, Montserrat_600SemiBold, Montserrat_600SemiBold_Italic, Montserrat_900Black_Italic, Montserrat_300Light, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold, Montserrat_800ExtraBold_Italic, Montserrat_700Bold_Italic, Montserrat_900Black} from "@expo-google-fonts/montserrat";
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Montserrat': require('./assets/fonts/Montserrat-VariableFont_wght.ttf'),
+    let [fontsLoaded, fontError] = useFonts({
+        Montserrat_900Black,
+        Montserrat_100Thin,
+        Montserrat_100Thin_Italic,
+        Montserrat_300Light_Italic,
+        Montserrat_200ExtraLight,
+        Montserrat_400Regular_Italic,
+        Montserrat_500Medium_Italic,
+        Montserrat_200ExtraLight_Italic,
+        Montserrat_800ExtraBold,
+        Montserrat_600SemiBold,
+        Montserrat_600SemiBold_Italic,
+        Montserrat_900Black_Italic,
+        Montserrat_300Light,
+        Montserrat_400Regular,
+        Montserrat_500Medium,
+        Montserrat_700Bold,
+        Montserrat_800ExtraBold_Italic,
+        Montserrat_700Bold_Italic,
     });
 
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text style={{left: wp('3%'), top: hp('2%'), fontFamily: "Montserrat", fontSize: wp('4.25%'), fontStyle: 'normal'}}>Explore Stores</Text>
-                <Text style={{left: wp('3%'), top: hp('1.75%'), fontFamily: "Montserrat", fontStyle: 'normal', fontWeight: 'bold', fontSize: wp('9%')}}>Near You</Text>
-                <View style={{left: wp('2.5%'), right: wp('2.5%'), top: hp('2%'), backgroundColor: '#D9D9D9', height: hp('0.4%'), width: wp('95%')}}></View>
-                <View style={{left: wp('4%'), right: wp('4%'), backgroundColor: '#F3F8FE', height: hp('5%'), top: hp('4%'), width: wp('92%'), borderRadius: wp('3.5%'), display: 'flex',  alignItems: 'center', flexDirection: 'row'}}>
-                    <AntDesign name="search1" size={wp('4%')} color="black" style={{left: wp('3%'), color: '#B8B8B8'}} />
-                    <Text style={{ left: wp('5%'), color: '#B8B8B8', fontSize: wp('4%')}}>Find stores...</Text>
-                </View>
-                {/* Filter switcher */}
-                <View>
-                    <View style={{top: hp('7%'), left: wp('4%'), display: 'flex', flexDirection: 'row', width: wp('70%'), justifyContent: 'center'}}>
-                        <View style={{top: hp('1.5%'), left: wp('10%'), height: hp('4%'), backgroundColor: '#f2f6fb', borderRadius: wp('4%'), width: wp('20%'), display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <Text style={{ color: '#176FF2', fontSize: wp('3.5%')}}>Location</Text>
+        <ScrollView>
+
+            <SafeAreaView>
+                    <View>
+                        <Text style={{left: wp('3%'), top: hp('2%'), fontFamily: "Montserrat_300Light", fontSize: wp('4.25%'), fontStyle: 'normal'}}>Explore Stores</Text>
+                        <Text style={{left: wp('2.5%'), top: hp('1.75%'), fontFamily: "Montserrat_400Regular", fontWeight: 400, fontSize: wp('9%')}}>Near You</Text>
+                        <View style={{left: wp('2.5%'), right: wp('2.5%'), top: hp('2%'), backgroundColor: '#D9D9D9', height: hp('0.4%'), width: wp('95%')}}></View>
+                        <View style={{left: wp('4%'), right: wp('4%'), backgroundColor: '#F3F8FE', height: hp('5%'), top: hp('4%'), width: wp('92%'), borderRadius: wp('3.5%'), display: 'flex',  alignItems: 'center', flexDirection: 'row'}}>
+                            <AntDesign name="search1" size={wp('4%')} color="black" style={{left: wp('3%'), color: '#B8B8B8'}} />
+                            <Text style={{ left: wp('5%'), color: '#B8B8B8', fontSize: wp('4%')}}>Find stores...</Text>
                         </View>
-                        <View style={{top: hp('1.5%'), left: wp('10%'), height: hp('4%'),  borderRadius: wp('4%'), width: wp('18%'), display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <Text style={{color: 'gray'}}>Type</Text>
+                        {/* Filter switcher */}
+                        <View>
+                            <View style={{top: hp('7%'), left: wp('4%'), display: 'flex', flexDirection: 'row', width: wp('70%'), justifyContent: 'center'}}>
+                                <View style={{top: hp('1.5%'), left: wp('10%'), height: hp('4%'), backgroundColor: '#f2f6fb', borderRadius: wp('4%'), width: wp('20%'), display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={{ color: '#176FF2', fontSize: wp('3.5%')}}>Location</Text>
+                                </View>
+                                <View style={{top: hp('1.5%'), left: wp('10%'), height: hp('4%'),  borderRadius: wp('4%'), width: wp('18%'), display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={{color: 'gray'}}>Type</Text>
+                                </View>
+                                <View style={{top: hp('1.5%'), left: wp('10%'), height: hp('4%'), borderRadius: wp('4%'), width: wp('18%'), display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={{color: 'gray'}}>Food</Text>
+                                </View>
+                            </View>
                         </View>
-                        <View style={{top: hp('1.5%'), left: wp('10%'), height: hp('4%'), borderRadius: wp('4%'), width: wp('18%'), display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <Text style={{color: 'gray'}}>Food</Text>
+                        {/* Points section */}
+                        <View style={{top: hp('13%')}}>
+                            <View style={{}}>
+                                <Text style={{left: wp('4.5%'), fontSize: wp('5%'), fontFamily: 'Montserrat_700Bold'}}>Your Points</Text>
+                                <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                    <Image source={require('./assets/no_stores_found.png')} resizeMethod={'resize'} style={{transform: 'scale(0.7)'}}></Image>
+                                </View>
+                                <Text style={{left: wp('28.5%'), top: hp('-4%'), fontSize: wp('5%'), fontWeight: 'bold', fontFamily: 'Montserrat_700Bold'}}>No points found</Text>
+                                <Text style={{top: hp('-3%'), fontSize: wp('4%'), fontFamily: 'Montserrat_400Regular', textAlign: 'center'}}>You have no points yet. Start shopping to earn points!</Text>
+                            </View>
                         </View>
+                        {/* Stores section */}
+                        <View style={{top: hp('15%')}}>
+                            <Text style={{left: wp('4.5%'), fontSize: wp('5%'), fontFamily: 'Montserrat_700Bold'}}>Near You</Text>
+                            <View style={{left: wp('3.5%'), top: hp('2%'), backgroundColor: '#eceaea', height: hp('16%'), width: wp('45%'), borderRadius: wp('5%')}}>
+                                <Image source={require('./assets/one.png')} resizeMethod={'resize'} style={{resizeMode: 'center', left: wp('-16%'), top: hp('-4%')}}></Image>
+                                <Text style={{top: hp('50%')}}>hi</Text>
+                            </View>
+                        </View>
+
                     </View>
-                </View>
-                {/* Points section */}
-                <View style={{top: hp('13%')}}>
-                    <View style={{}}>
-                        <Text style={{left: wp('4.5%'), fontSize: wp('5%'), fontFamily: 'Montserrat'}}>Your Points</Text>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <Image source={require('./assets/no_stores_found.png')} resizeMethod={'resize'} style={{transform: 'scale(0.7)', left: wp('7%')}}></Image>
-                        </View>
-                    </View>
-                </View>
-            </View>
-        </SafeAreaView>
-  );
-}
+            </SafeAreaView>
+    </ScrollView>
+
+);}
 
 const styles = StyleSheet.create({
 
